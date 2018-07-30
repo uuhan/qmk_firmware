3e4cb786fee40d6512a8e65d1278022119fbd7fc
徐敏辉 <minhuixu@clubfactory.com>
2018-07-30 18:58:14 +0800

    WIP

--- keyboards/hhkb/keymaps/blakedietz/config.h
+++ keyboards/hhkb/keymaps/blakedietz/config.h
@@ -20,5 +20,7 @@
 // This makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when
 // you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 #define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TOGGLE 2
+#define PERMISSIVE_HOLD
 
 #endif
