9a60c57c4d13eaeafa0f5f0c6e5028c332eae38f
uuhan <xuminhui189@gmail.com>
2018-08-10 13:56:38 +0800

    style

--- keyboards/hhkb/keymaps/uuhan/keymap.c
+++ keyboards/hhkb/keymaps/uuhan/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         GUI_T(KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
         CTL_T(KC_ESC), LT(_MOUSE_L, KC_A), LT(_OTHER, KC_S), KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(_MOUSE_R, KC_SCLN), KC_QUOT, MT(KC_RCTL, KC_ENT),
         OSM(MOD_LSFT), GUI_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, MT(KC_RGUI, KC_SLSH), MT(KC_RSFT, KC_ESC), TT(HHKB),
-                        KC_LALT, LT(_MIRROR, KC_GRV), LT(_SPACEFN, KC_SPC), TT(_MIRROR), KC_RALT),
+                            KC_LALT, LT(_MIRROR, KC_GRV), LT(_SPACEFN, KC_SPC), TT(_MIRROR), KC_RALT),
 
     [_HHKB] = LAYOUT(
         KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_F7, KC_F8, KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC,
         KC_LCTL, KC_F9, KC_TRNS, KC_BSPC, KC_DEL, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_MUTE, KC_TRNS, KC_MPLY, KC_ENT,
         KC_LSFT, KC_F10, KC_F11, KC_F12, KC_F13, KC_F14, KC_TRNS, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_RSFT, KC_TRNS,
-        KC_LGUI, KC_LALT, KC_ENT, KC_RGUI, KC_RALT)};
+                                KC_LGUI, KC_LALT, KC_ENT, KC_RGUI, KC_RALT)};
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
