7439bde116f106b8c00dd1b3e437b644c2bfc237
uuhan <xuminhui189@gmail.com>
2018-08-12 03:14:25 +0800

    wip

--- keyboards/niu_mini/keymaps/uuhan/keymap.c
+++ keyboards/niu_mini/keymaps/uuhan/keymap.c
@@ -127,10 +127,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_SPACEFN] = LAYOUT_planck_mit(
-  KC_BSPC, KC_LBRC, KC_RBRC, KC_UP  , KC_PIPE, KC_TAB, KC_J , KC_L   , KC_U   , KC_Y  , KC_SCLN, KC_TRNS,
-  KC_ENT , KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_D  , KC_H , KC_N   , KC_E   , KC_I  , KC_O   , KC_QUOT,
-  KC_LSFT, KC_RPRN, KC_LCBR, KC_RCBR, KC_V   , KC_B  , KC_K , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_ENT ,
-  KC_LCTL, KC_LGUI, KC_LALT, BACKLIT, RAISE  , KC_SPC, LOWER, KC_LEFT, KC_DOWN, KC_UP , KC_RGHT
+  KC_BSPC, KC_LBRC, KC_RBRC, KC_UP  , KC_PIPE, KC_TAB , KC_GRV , KC_L   , KC_U   , KC_Y  , KC_SCLN, KC_TRNS,
+  KC_ENT , KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_MINS, KC_EQL , KC_N   , KC_E   , KC_I  , KC_O   , KC_QUOT,
+  KC_LSFT, KC_RPRN, KC_LCBR, KC_RCBR, KC_BSLS, KC_UNDS, KC_PLUS, KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_ENT ,
+  KC_LCTL, KC_LGUI, KC_LALT, BACKLIT, RAISE  , KC_SPC , LOWER, KC_LEFT, KC_DOWN, KC_UP , KC_RGHT
 ),
 
 [_FNKEYS] = LAYOUT_planck_mit(
