e3536d3ee6af98a16f3e7211bc1ffbe1ae09d73c
uuhan <xuminhui189@gmail.com>
2018-08-11 23:34:04 +0800

    FNKEYS

--- keyboards/niu_mini/keymaps/uuhan/keymap.c
+++ keyboards/niu_mini/keymaps/uuhan/keymap.c
@@ -27,6 +27,7 @@ enum layers {
   _QWERTY,
   _MOUSE,
   _SPACEFN,
+  _FNKEYS,
   _DVORAK,
   _LOWER,
   _RAISE,
@@ -38,6 +39,7 @@ enum layers {
 enum keycodes {
   QWERTY = SAFE_RANGE,
   SPACEFN,
+  FNKEYS,
   DVORAK,
   PLOVER,
   LOWER,
@@ -82,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_mit(
   ALT_T(KC_TAB), KC_Q            , KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  CTL_T(KC_ESC), LT(_MOUSE, KC_A), KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_SCLN_QUOT), MT(MOD_RCTL, KC_ENT),
+  CTL_T(KC_ESC), LT(_MOUSE, KC_A), LT(_FNKEYS, KC_S),    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_SCLN_QUOT), MT(MOD_RCTL, KC_ENT),
   OSM(MOD_LSFT), GUI_T(KC_Z)     , KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_QUOT),
   KC_LALT      , KC_LGUI         , KC_LGUI, GUI_T(KC_GRV), LT(_RAISE, KC_ENT), LT(_SPACEFN, KC_SPC), LT(_LOWER, KC_BSPC),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
@@ -121,6 +123,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ENT , KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_D  , KC_H , KC_N   , KC_E   , KC_I  , KC_O   , KC_QUOT,
   KC_LSFT, KC_RPRN, KC_LCBR, KC_RCBR, KC_V   , KC_B  , KC_K , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_ENT ,
   KC_LCTL, KC_LGUI, KC_LALT, BACKLIT, RAISE  , KC_SPC, LOWER, KC_LEFT, KC_DOWN, KC_UP , KC_RGHT
+),
+
+[_FNKEYS] = LAYOUT_planck_mit(
+  KC_BSLS, KC_EQL , KC_MINS, KC_HOME, KC_UP  , KC_DOWN, KC_END, KC_L   , KC_U   , KC_Y  , KC_SCLN, KC_TRNS,
+  KC_ENT , KC_LPRN, _______, KC_BSPC, KC_DEL , KC_D   , KC_H  , KC_N   , KC_E   , KC_I  , KC_O   , KC_QUOT,
+  KC_LSFT, KC_RPRN, KC_LCBR, KC_RCBR, KC_V   , KC_B   , KC_K  , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_ENT ,
+  KC_LCTL, KC_LGUI, KC_LALT, BACKLIT, RAISE  , KC_ENT , LOWER , KC_LEFT, KC_DOWN, KC_UP , KC_RGHT
 ),
 
 /* Dvorak
