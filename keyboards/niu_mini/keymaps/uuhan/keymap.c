5851a384386d0d5f840c687701d4888830847e25
uuhan <xuminhui189@gmail.com>
2018-08-11 21:41:53 +0800

    KC_0

--- keyboards/niu_mini/keymaps/uuhan/keymap.c
+++ keyboards/niu_mini/keymaps/uuhan/keymap.c
@@ -153,10 +153,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_mit(
-  KC_NLCK, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,          KC_BSPC,
-  KC_DEL,  KC_4,    KC_5,    KC_6,    _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_GRV,
+  KC_0   , KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,          KC_BSPC,
+  KC_DEL , KC_4,    KC_5,    KC_6,    _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_GRV,
   _______, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______, KC_LT ,  KC_GT  , KC_BSLS, _______,
-  _______, _______, _______, KC_0,    _______,     _______,      _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  _______, _______, _______, _______, _______,     _______,      _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Lower
