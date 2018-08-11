34267d7f8c2cb5306d9d7949d1190ab4ff95b662
uuhan <xuminhui189@gmail.com>
2018-08-11 23:03:42 +0800

    LTt

--- keyboards/niu_mini/keymaps/uuhan/keymap.c
+++ keyboards/niu_mini/keymaps/uuhan/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ALT_T(KC_TAB), KC_Q            , KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   CTL_T(KC_ESC), LT(_MOUSE, KC_A), KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_SCLN_QUOT), KC_ENT,
   OSM(MOD_LSFT), GUI_T(KC_Z)     , KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_QUOT),
-  KC_LALT      , KC_LGUI         , KC_LGUI, KC_LGUI, RAISE,   LT(_SPACEFN, KC_SPC),      LOWER,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_LALT      , KC_LGUI         , KC_LGUI, KC_LGUI, LT(_RAISE, KC_ENT), LT(_SPACEFN, KC_SPC), LT(_LOWER, KC_BSPC),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Mouse
@@ -156,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_0   , KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,          KC_BSPC,
   KC_DEL , KC_4,    KC_5,    KC_6,    _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_GRV,
   _______, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______, KC_LT ,  KC_GT  , KC_BSLS, _______,
-  _______, _______, _______, _______, _______,     _______,      _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  _______, _______, _______, _______, _______,     _______,      LOWER, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Lower
@@ -174,7 +174,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TILD,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, KC_PIPE, _______,
-  _______, _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END
+  _______, _______, _______, _______, RAISE,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END
 ),
 
 /* Plover layer (http://opensteno.org)
