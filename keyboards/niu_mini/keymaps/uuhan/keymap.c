74a0a8c6d796e14c06ef87f2213a15193100b6b0
uuhan <xuminhui189@gmail.com>
2018-08-12 13:45:38 +0800

    WIP

--- keyboards/niu_mini/keymaps/uuhan/keymap.c
+++ keyboards/niu_mini/keymaps/uuhan/keymap.c
@@ -171,9 +171,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_mit(
-  KC_ESC , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7   , KC_8  ,  KC_9   , KC_0   ,  KC_DEL,
-  KC_ENT , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_TILD,
-  F(0)   , KC_MINS, KC_EQL , KC_BSLS, KC_GRV,  KC_BSPC, _______, _______, KC_LT ,  KC_GT  , KC_BSLS, _______,
+  ALT_T(KC_ESC), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7   , KC_8  ,  KC_9   , KC_0   ,  KC_DEL,
+  GUI_T(KC_ENT), KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_TILD,
+  F(0)         , KC_MINS, KC_EQL , KC_BSLS, KC_GRV,  KC_BSPC, _______, _______, KC_LT ,  KC_GT  , KC_BSLS, _______,
   _______, _______, _______, _______, _______,     _______,      LOWER, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
