dce64b8a5b61ca29bccbac00dcece22fb6d8f41a
uuhan <xuminhui189@gmail.com>
2018-08-12 21:55:41 +0800

    fix lower raise

--- keyboards/niu_mini/keymaps/uuhan/keymap.c
+++ keyboards/niu_mini/keymaps/uuhan/keymap.c
@@ -171,10 +171,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_mit(
-  ALT_T(KC_ESC), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7   , KC_8   ,  KC_9   , KC_0   ,  KC_DEL,
-  GUI_T(KC_ENT), KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, KC_MINS, KC_EQL ,  KC_LBRC, KC_RBRC,  KC_GRV,
-  F(0)         , KC_MINS, KC_EQL , KC_BSLS, KC_GRV,  KC_BSPC, _______, _______, _______,  _______, KC_BSLS, _______,
-  _______, _______, _______, _______, _______,     _______,     LOWER, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  ALT_T(KC_ESC) , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7   , KC_8   ,  KC_9   , KC_0   ,  KC_DEL,
+  GUI_T(KC_BSPC), KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, KC_MINS, KC_EQL ,  KC_LBRC, KC_RBRC,  KC_GRV,
+  F(0)          , KC_MINS, KC_EQL , KC_BSLS, KC_GRV,  KC_BSPC, _______, _______, _______,  _______, KC_BSLS, _______,
+  _______, _______, _______, _______, _______,     _______,     RAISE, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Lower
@@ -192,7 +192,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TILD,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_LT  , KC_GT  , KC_PIPE, KC_DQUO,
-  _______, _______, _______, _______, RAISE,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END
+  _______, _______, _______, _______, _______,     _______,      LOWER, KC_HOME, KC_PGDN, KC_PGUP,  KC_END
 ),
 
 /* Plover layer (http://opensteno.org)
