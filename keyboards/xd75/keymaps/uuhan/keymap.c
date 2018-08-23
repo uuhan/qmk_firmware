37e62e4c6518ced6f38d551f20c9121102b9960e
uuhan <xuminhui189@gmail.com>
2018-08-23 21:27:51 +0800

    mouse lifted keys

--- keyboards/xd75/keymaps/uuhan/keymap.c
+++ keyboards/xd75/keymaps/uuhan/keymap.c
@@ -1,5 +1,13 @@
 #include "xd75.h"
 
+#include "quantum.h"
+#include "action.h"
+#include "mousekey.h"
+#include "process_keycode/process_tap_dance.h"
+
+extern keymap_config_t keymap_config;
+
+
 #ifdef IS_COLINTA
 #include "secrets.h"
 #else
@@ -66,16 +74,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC       , TH_F1                   , TH_F2, TH_F3   , TH_F4               , TH_F5 , MM_0  , MM_1    , KC_DEL , TH_F6 , TH_F7 , TH_F8  , TH_F9 , TH_F10 , KC_BSLS,
     GUI_T(KC_TAB), KC_Q                    , KC_W , KC_E    , KC_R                , KC_T  , MM_2  , GOTO_FN , _____  , KC_Y  , KC_U  , KC_I   , KC_O  , KC_P   , KC_BSPC,
     CTL_T(KC_ESC), LT(LAYER_MOUSE_L, KC_A) , KC_S , KC_D    , KC_F                , KC_G  , _____ , _____   , _____  , KC_H  , KC_J  , KC_K   , KC_L  , KC_SCLN, KC_ENT ,
-    KC_LSFT      , KC_Z                    , KC_X , KC_C    , KC_V                , KC_B  , _____ , _____   , _____  , KC_N  , KC_M  , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+    KC_LSFT      , GUI_T(KC_Z)             , KC_X , KC_C    , KC_V                , KC_B  , _____ , _____   , _____  , KC_N  , KC_M  , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
     KC_LALT      , _____                   , _____, MOD_LALT, LT(LAYER_FN, KC_TAB), KC_SPC, _____ , _____   , _____  , _____ , _____ , _____  , _____ , _____  , _____
   ),
 
   [LAYER_MOUSE_L] = KEYMAP(
-    KC_ESC       , TH_F1, TH_F2  , TH_F3  , TH_F4  , TH_F5  , MM_0   , MM_1   , KC_DEL, TH_F6, TH_F7, TH_F8, TH_F9, TH_F10 , KC_BSLS,
-    CTL_T(KC_ESC), KC_Q , KC_WH_U, KC_MS_U, KC_WH_D, KC_LGUI, MM_2   , GOTO_FN, _____ , KC_Y , KC_U , KC_I , KC_O , KC_P   , _____  ,
-    KC_LSFT      , _____, KC_MS_L, KC_MS_D, KC_MS_R, KC_LCTL, _____  , _____  , _____ , KC_H , KC_J , KC_K , KC_L , KC_SCLN, _____  ,
-    KC_LCTL      , KC_Z , KC_X   , KC_C   , KC_V   , KC_LALT, _____  , _____  , _____ , KC_N , KC_M , _____, _____, _____  , _____  ,
-    _____        , _____, _____  , _____  , KC_BTN2, KC_BTN1, KC_BTN3, _____  , _____ , _____, _____, _____, _____, _____  , _____
+    KC_ESC       , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, _____  , _____  , _____, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+    CTL_T(KC_ESC), KC_Q   , KC_WH_U, KC_MS_U, KC_WH_D, KC_LGUI, MM_2   , GOTO_FN, _____, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , _____  ,
+    KC_LSFT      , _____  , KC_MS_L, KC_MS_D, KC_MS_R, KC_LCTL, _____  , _____  , _____, KC_H   , KC_BTN1, KC_BTN2, KC_BTN3, KC_SCLN, _____  ,
+    KC_LCTL      , KC_Z   , KC_X   , KC_C   , KC_V   , KC_LALT, _____  , _____  , _____, KC_N   , KC_M   , _____  , _____  , _____  , _____  ,
+    _____        , _____  , _____  , _____  , KC_BTN2, KC_BTN1, KC_BTN3, _____  , _____, _____  , _____  , _____  , _____  , _____  , _____
   ),
 
 /* COLEMAK
