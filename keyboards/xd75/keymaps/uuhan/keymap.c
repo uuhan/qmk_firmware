654d1e05b03535b0916f15a9ee37e4d4f475d54f
uuhan <xuminhui189@gmail.com>
2018-08-23 23:36:04 +0800

    fn issue

--- keyboards/xd75/keymaps/uuhan/keymap.c
+++ keyboards/xd75/keymaps/uuhan/keymap.c
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO,          KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO,          KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO,          KC_NO,      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO,   KC_NO,   KC_NO, KC_NO, RESET,    KC_NO, DM_CLEAR, KC_NO,             KC_NO, KC_NO, RESET, KC_NO, KC_NO, KC_NO, KC_NO
+    KC_NO,   KC_NO,   KC_NO, KC_NO, _____, KC_NO,     DM_CLEAR,       KC_NO,      KC_NO, KC_NO, RESET, KC_NO, KC_NO, KC_NO, KC_NO
   )
 };
 
