02726acfc5af2cc46f62a75b4163bd76a92968cf
徐敏辉 <minhuixu@clubfactory.com>
2018-08-29 16:39:40 +0800

    add fn layout

--- keyboards/niu_mini/keymaps/uuhan/keymap.c
+++ keyboards/niu_mini/keymaps/uuhan/keymap.c
@@ -30,6 +30,7 @@ void send_keycode(uint8_t kc){
 
 enum layers {
   _QWERTY,
+  _FNS,
   _MACRO,
   _MOUSE_L,
   _MOUSE_R,
@@ -116,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_mit(
-  SH_T(KC_TAB)   , KC_Q              , KC_W             , KC_E       , KC_R, KC_T, KC_Y, KC_U, KC_I   , KC_O  , KC_P       , KC_BSPC              ,
+  SH_T(KC_TAB)   , KC_Q              , KC_W             , KC_E       , KC_R, KC_T, KC_Y, KC_U, KC_I   , KC_O  , KC_P       , LT(_FNS, KC_BSPC)    ,
   LCTL_T(KC_ESC) , LT(_MOUSE_L, KC_A), LT(_FNKEYS, KC_S), KC_D       , KC_F, KC_G, KC_H, KC_J, KC_K   , KC_L  , TD(TD_SCLN), MT(MOD_RCTL , KC_ENT),
   OSM(MOD_LSFT)  , GUI_T(KC_Z)       , CTL_T(KC_X)      , ALT_T(KC_C), KC_V, KC_B, KC_N, KC_M, RALT_T(KC_COMM), RCTL_T(KC_DOT), TD(TD_SLSH), TD(TD_QUOT_SHIFT)    ,
   KC_LALT        , MO(_MACRO)        , SH_TT            , GUI_T(KC_GRV), TD(TD_LOWER), TD(TD_SPACE), TD(TD_RAISE), KC_MINS, KC_EQL, DYN_MACRO_PLAY2, DYN_MACRO_PLAY1
@@ -126,6 +127,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
     RGB_MOD, RGB_TOG, DYN_REC_STOP, DYN_REC_START1, DYN_REC_START2, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, BL_TOGG, BL_STEP, RGB_HUI, RGB_HUD, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
+),
+
+[_FNS] = LAYOUT_planck_mit(
+    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5 , KC_F6 , KC_F7 , KC_F8 , KC_F9 , KC_F10, _______,
+    _______, KC_F11 , KC_F12 , KC_F13 , KC_F14 , KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
 ),
 
