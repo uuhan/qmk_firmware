7068363ad5d0754e9baf4194a5e21e714f7beaeb
uuhan <xuminhui189@gmail.com>
2018-09-16 11:41:41 +0800

    add delay to wheel scroll

--- keyboards/hhkb/keymaps/uuhan/config.h
+++ keyboards/hhkb/keymaps/uuhan/config.h
@@ -11,7 +11,7 @@
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_MAX_SPEED      3
 #define MOUSEKEY_TIME_TO_MAX    5
-#define MOUSEKEY_WHEEL_DELAY    0
+#define MOUSEKEY_WHEEL_DELAY    200
 
 #ifdef TAPPING_TERM
   #undef TAPPING_TERM
