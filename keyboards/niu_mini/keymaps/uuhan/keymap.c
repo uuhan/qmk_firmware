fc2ce53726ca6a7c712b40b1f3b00fc840b672af
uuhan <xuminhui189@gmail.com>
2018-12-22 12:06:09 +0800

    布局调整

--- keyboards/niu_mini/keymaps/uuhan/keymap.c
+++ keyboards/niu_mini/keymaps/uuhan/keymap.c
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_FNKEYS] = LAYOUT_planck_mit(
-  KC_BSLS       , KC_F5 , _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END , KC_UNDS, KC_PLUS, KC_PIPE, KC_TILD, KC_BSPC,
+  KC_BSLS       , KC_F5 , _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END , KC_UNDO, KC_UNDS, KC_PLUS, KC_PIPE, KC_TILD,
   CTL_T(KC_ENT) , KC_F6 , _______, KC_BSPC, KC_DEL , KC_F7  , KC_F8  , KC_MINS, KC_EQL , KC_BSLS, KC_GRV , _______,
   KC_LSFT, KC_F9, KC_F10, KC_F11 , KC_F12 , KC_F13 , KC_F14 , KC_F15 , _______, _______, _______, _______,
   _______, _______, _______, _______, KC_ENT ,      KC_ENT     , KC_TAB , _______, _______, _______, _______
