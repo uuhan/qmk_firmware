011888b69fcec5bf5c7b08ab91dae9042e4b2c9a
uuhan <xuminhui189@gmail.com>
2019-01-08 09:53:29 +0800

    macro_start1

--- keyboards/niu_mini/keymaps/uuhan/keymap.c
+++ keyboards/niu_mini/keymaps/uuhan/keymap.c
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_MACRO] = LAYOUT_planck_mit(
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    DYN_REC_START1,
     RGB_MOD, RGB_TOG, DYN_REC_STOP, DYN_REC_START2, DYN_REC_START1, _______, _______, DYN_MACRO_PLAY1, DYN_REC_START2, _______, _______, _______,
     _______, BL_TOGG, BL_STEP, RGB_HUI, RGB_HUD, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
